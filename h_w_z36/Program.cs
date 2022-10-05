495730039ecbf7c04dd46f95909be2c1989b5c72
lplnt <phd@live.ru>
2022-10-05 23:42:35 +0300

    home work lessen 5

---
+++ h_w_z36/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+int[] arr = new int[4];
+Random rnd = new Random();
+Console.Write("Случайный массив:");
+for (int i = 0; i < 4; i++)
+{
+    arr[i] = rnd.Next(-99, 100);
+    Console.Write("{0} ", arr[i]);
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма нечетных позиций = {arr[1] + arr[3]}");
---
+++ h_w_z38/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+int[] arr = new int[5];
+Random rnd = new Random();
+
+int n = 5;
+int j = 0;
+for (int i = 0; i < n; i++)
+{
+    arr[i] = rnd.Next(0, 99);
+    Console.Write("{0} ", arr[i]);
+}
+Console.WriteLine();
+var min = arr[0];
+var max = arr[0]; 
+var minIndex = 0;
+var maxIndex = 0;
+ 
+for (j = 0; j < n; j++)
+{
+    if (arr[j] < min)
+    {
+        min = arr[j];
+        minIndex = j;
+    }
+    else if (arr[j] > max)
+    {
+        max = arr[j];
+        maxIndex = j;
+    }
+}
+Console.WriteLine($"Разница между мин и макс = {max - min}");
+Console.WriteLine();
